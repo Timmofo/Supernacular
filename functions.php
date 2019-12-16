30607331662bdcd759413ec7e6ba02f26f0d38ef
Rick Lee <rick.lee0629@hotmail.com% git config --global user.email rick.lee0629@hotmail.com>
2019-12-16 17:32:08 +0800

    Fix typo

--- functions.php
+++ functions.php
@@ -1,8 +1,8 @@
 <?php
 
 function theme_files() {
-    wp_enqueue_script('google-font', '//fonts.googleapis.com/css?family=Crimson+Pro:600|Lato:400,700&display=swap');
-    wp_enqueue_script('main_styles', get_stylesheet_uri());
+    wp_enqueue_style('google-font', '//fonts.googleapis.com/css?family=Crimson+Pro:600|Lato:400,700&display=swap');
+    wp_enqueue_style('main_style', get_stylesheet_uri());
 }
 
 add_action('wp_enqueue_scripts', 'theme_files');
@@ -10,7 +10,7 @@ add_action('wp_enqueue_scripts', 'theme_files');
 function theme_features() {
     register_nav_menu('headerMenu', 'Header Menu');
     register_nav_menu('footerMenu', 'Footer Menu');
-    register_nav_menu('footerLegal', 'FooterLegal');
+    register_nav_menu('footerLegal', 'Footer Legal');
 
     add_theme_support('title-tag');
 }
