52cf5d9deba13207fc9b612910c20119bca39da8
Rick Lee <rick.lee0629@hotmail.com% git config --global user.email rick.lee0629@hotmail.com>
2019-12-23 15:22:02 +0800

    Add footer

--- template-parts/footer/footer.php
+++ template-parts/footer/footer.php
@@ -1,4 +1,12 @@
-    <!-- FOOTER HERE -->
+    <footer class="container">
+        <div class="footer-content">
+            <img src="#" alt="">
+            <p>All Rights Reserved &copy; 2019 Supernacular </p>
+        </div>
+        <div class="footer-sitemap">
+            <?php dynamic_sidebar('footer'); ?>
+        </div>
+    </footer>
 
     <?php wp_footer(); ?>
 </body>
