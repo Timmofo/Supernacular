6869aead9e4c8746ad46b55f8fc412ca4c48a999
Rick Lee <rick.lee0629@hotmail.com% git config --global user.email rick.lee0629@hotmail.com>
2019-12-30 17:02:21 +0800

    Add sidebar menu

--- template-parts/header/header.php
+++ template-parts/header/header.php
@@ -8,16 +8,16 @@
 </head>
 <body <?php body_class(); ?>>
 
-    <!-- <div id="nav-side" class="sidebar">
+    <div id="nav-side" class="sidebar">
         <a href="javascript:void(0)" class="btn-collapse" onclick="collapse()">&times;</a>
-        <?php
-        
+        <a href="<?php echo home_url(); ?>">Home</a>
+        <?php 
         $menu = wp_get_nav_menu_items('Header'); 
 
-        foreach ($menu as $menu_item): ?>
-            <a href="<?= $menu_item->url ?>"><?= $menu_item->title ?></a>
+            foreach ($menu as $menu_item): ?>
+                <a href="<?= $menu_item->url ?>"><?= $menu_item->title ?></a>
         <?php endforeach; ?>
-    </div> -->
+    </div>
     
     <header class="container" <?php if (is_front_page()) echo "style='height: 100vh;'"?>>
         <nav id="nav" class="container <?php if (is_front_page()) echo "top-of-page"?>">
@@ -34,5 +34,5 @@
             ));
             ?>
 
-            <!-- <button class="btn-expand" onclick="expand()">&#9776;</button> -->
+            <button class="btn-expand" onclick="expand()">&#9776;</button>
         </nav>
