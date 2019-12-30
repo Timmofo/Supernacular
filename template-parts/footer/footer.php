f49dea99cfb92a1a8d37570e066914f72045a7f1
Rick Lee <rick.lee0629@hotmail.com% git config --global user.email rick.lee0629@hotmail.com>
2019-12-30 17:02:49 +0800

    Add collapsible footer menus

--- template-parts/footer/footer.php
+++ template-parts/footer/footer.php
@@ -5,6 +5,32 @@
         <div class="footer-sitemap">
             <?php dynamic_sidebar('footer-sitemap'); ?>
         </div>
+
+        <div class="footer-sitemap-collapsible">
+            <button type="button" class="collapsible">
+                <h3>Explore</h3>
+            </button>
+            <div class="content">
+            <?php 
+            $menu = wp_get_nav_menu_items('Footer Menu'); 
+
+                foreach ($menu as $menu_item): ?>
+                    <a href="<?= $menu_item->url ?>"><?= $menu_item->title ?></a>
+            <?php endforeach; ?>
+            </div>
+
+            <button type="button" class="collapsible">
+                <h3>Legal</h3>
+            </button>
+            <div class="content">
+                <?php 
+                $menu = wp_get_nav_menu_items('Footer Legal'); 
+
+                    foreach ($menu as $menu_item): ?>
+                        <a href="<?= $menu_item->url ?>"><?= $menu_item->title ?></a>
+                <?php endforeach; ?>
+            </div>
+        </div>
     </footer>
 
     <?php wp_footer(); ?>
