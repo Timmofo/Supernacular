c6401adc066cb38050803f3e869611c28c39a9dd
Rick Lee <rick.lee0629@hotmail.com% git config --global user.email rick.lee0629@hotmail.com>
2019-12-30 12:49:59 +0800

    Enqueue font-awesome icon kit

--- functions.php
+++ functions.php
@@ -2,6 +2,7 @@
 
 function theme_files() {
     wp_enqueue_script('main_js', get_template_directory_uri().'/assets/js/script.js', array('jquery'), 1.0, true);
+    wp_enqueue_script('font_awesome', '//kit.fontawesome.com/74bf3eb8e8.js');
     wp_enqueue_style('google-font', '//fonts.googleapis.com/css?family=Crimson+Pro:600|Lato:400,700&display=swap');
     wp_enqueue_style('main_style', get_template_directory_uri().'/assets/css/styles.css');
 }
@@ -35,3 +36,18 @@ function theme_widgets() {
 }
 
 add_action( 'widgets_init', 'theme_widgets' );
+
+// OPTIONAL
+
+function theme_custom_logo() {
+    $defaults = array(
+    'height'      => 100,
+    'width'       => 100,
+    'flex-height' => true,
+    'flex-width'  => true,
+    'header-text' => array( 'site-title', 'site-description' ),
+    );
+    add_theme_support( 'custom-logo', $defaults );
+}
+
+add_action( 'after_setup_theme', 'theme_custom_logo' );
