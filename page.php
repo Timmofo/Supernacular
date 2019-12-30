4928ded7db579ae5b5245740c58214f2923ace98
Rick Lee <rick.lee0629@hotmail.com% git config --global user.email rick.lee0629@hotmail.com>
2019-12-30 12:48:26 +0800

    Add default page template

---
+++ page.php
@@ -0,0 +1,20 @@
+<?php get_template_part('template-parts/header/header'); ?>
+
+    </header>
+
+    <?php if (have_posts()) {
+        the_post(); ?>
+
+        <main class="container">
+            <section class="section-title">
+                <h1><?php the_title(); ?></h1>
+            </section>
+
+            <section>
+                <?php the_content(); ?>
+            </section>
+        </main>
+
+    <?php } ?>
+
+<?php get_template_part('template-parts/footer/footer'); ?>
