106bc8e3fb02f88916b800a987b310b715b929d0
Rick Lee <rick.lee0629@hotmail.com% git config --global user.email rick.lee0629@hotmail.com>
2019-12-30 09:02:39 +0800

    Add scripts for switching between logos

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,13 @@
-// CHECK FOR PAGE SCROLLING, ADD/REMOVE CLASS TO TOP NAVIGATION
+// // SET INITIAL STATE TO TOP NAVIGATION ON REFRESH
+// jQuery(window).ready(function() {
 
+//     // CHECK FOR FRONT PAGE
+//     if (jQuery('body.home').length) {
+//         jQuery('#nav').addClass('top-of-page');
+//     }
+// });
+
+// CHECK FOR PAGE SCROLLING, THEN ADD/REMOVE STATE TO TOP NAVIGATION
 jQuery(window).on("scroll", function() {
     var scrollPos = jQuery(window).scrollTop();
 
@@ -7,8 +15,10 @@ jQuery(window).on("scroll", function() {
     if (jQuery('body.home').length) {
         if (scrollPos <= 0) {
             jQuery('#nav').addClass('top-of-page');
+            jQuery('.logo').attr("src", "http://supernacular.local/wp-content/themes/Supernacular/assets/images/supernacular-logo-white.svg");
         } else {
             jQuery('#nav').removeClass('top-of-page');
+            jQuery('.logo').attr("src", "http://supernacular.local/wp-content/themes/Supernacular/assets/images/supernacular-logo-full.svg");
         }
     }
 });
