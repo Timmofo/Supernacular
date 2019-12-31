e93c6cb8bf14059ed5ab512e97cdac7ff24de94a
Rick Lee <rick.lee0629@hotmail.com% git config --global user.email rick.lee0629@hotmail.com>
2019-12-31 21:03:26 +0800

    Fix content

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,6 @@
-// // SET INITIAL STATE TO TOP NAVIGATION ON REFRESH
-// jQuery(window).ready(function() {
-
-//     // CHECK FOR FRONT PAGE
-//     if (jQuery('body.home').length) {
-//         jQuery('#nav').addClass('top-of-page');
-//     }
-// });
-
-
+// COLLAPSIBLE FULLSCREEN MENU
 function expand() {
-    document.getElementById("nav-side").style.width = "250px";
+    document.getElementById("nav-side").style.width = "100%";
 }
   
 function collapse() {
@@ -24,22 +15,23 @@ jQuery(window).on("scroll", function() {
     if (jQuery('body.home').length) {
         if (scrollPos <= 0) {
             jQuery('#nav').addClass('top-of-page');
-            jQuery('.logo').attr("src", "http://supernacular.local/wp-content/themes/Supernacular/assets/images/supernacular-logo-white.svg");
+            jQuery('#nav .logo').attr("src", "http://supernacular.local/wp-content/themes/Supernacular/assets/images/supernacular-logo-white.svg");
         } else {
             jQuery('#nav').removeClass('top-of-page');
-            jQuery('.logo').attr("src", "http://supernacular.local/wp-content/themes/Supernacular/assets/images/supernacular-logo-full.svg");
+            jQuery('#nav .logo').attr("src", "http://supernacular.local/wp-content/themes/Supernacular/assets/images/supernacular-logo-full.svg");
         }
     }
 });
 
-// ACCORDION
+// COLLAPSIBLE FOOTER MENU
 
-var coll = document.getElementsByClassName("collapsible");
+var collapse = document.getElementsByClassName("collapsible");
 var i;
 
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
+for (i = 0; i < collapse.length; i++) {
+  collapse[i].addEventListener("click", function() {
     this.classList.toggle("active");
+    
     var content = this.nextElementSibling;
     if (content.style.display === "flex") {
       content.style.display = "none";
