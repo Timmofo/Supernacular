6256811ecc5a0f2c97bed259cde0260b508e2e33
Rick Lee <rick.lee0629@hotmail.com% git config --global user.email rick.lee0629@hotmail.com>
2020-01-02 18:05:40 +0800

    Rename variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,11 +25,11 @@ jQuery(window).on("scroll", function() {
 
 // COLLAPSIBLE FOOTER MENU
 
-var collapse = document.getElementsByClassName("collapsible");
+var coll = document.getElementsByClassName("collapsible");
 var i;
 
-for (i = 0; i < collapse.length; i++) {
-  collapse[i].addEventListener("click", function() {
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
     
     var content = this.nextElementSibling;
